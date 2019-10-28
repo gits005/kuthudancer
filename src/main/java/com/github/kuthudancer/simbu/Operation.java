6fab514318b6462fea5a9056836d3310a51a0d25
gits005 <57099409+gits005@users.noreply.github.com>
2019-10-28 18:49:47 +0530

    Add files via upload

---
+++ src/main/java/com/github/kuthudancer/simbu/Operation.java
@@ -0,0 +1,16 @@
+package com.github.kuthudancer.simbu;
+
+/**
+ * Hello world!
+ *
+ */
+public class Operation 
+{
+    public static void main( String[] args )
+    {
+    	for (int i=1;i<5;i++)
+    	{
+        System.out.println( "Hello World!" );
+    	}
+    }
+}
